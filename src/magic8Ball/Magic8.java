574f3f0a9538dd7f34a8d582d11bb689b68bb6e9
Haley <Haley.boyles@opsknights.com>
2015-10-29 09:45:36 -0400

    ok

--- src/magic8Ball/Magic8.java
+++ src/magic8Ball/Magic8.java
@@ -1,18 +1,21 @@
-package magic8Ball;
-import java.util.Random;
-
-public class Magic8 {
-
-	public static void main(String[] args) {
-		String question = "Your question here";
-		System.out.println(question);
-		
-		// Fill the array with at least five potential answers
-		String[] answers = new String[5];
+ package magic8Ball;
+ import java.util.Random;
+import java.util.Scanner;
+ 
+ public class Magic8 {
+ 
+ 	public static void main(String[] args) {
+		Scanner keybrd = new Scanner(System.in);
+ 		String question = "What would you like to know?";
+ 		System.out.println(question);
+ 		String person = keybrd.nextLine();
+ 		
+ 		// Fill the array with at least five potential answers
+		String[] answers= {"yes","no","try again later","not sure","Maybe","Most Likely","Probably Not"};
 		
 		// Choose one of the answers at random, then print it
-		System.out.println();
+		int random= (int) (Math.random()*6);
+		System.out.print(answers[random]);
 
 	}
-
-}
+	}
